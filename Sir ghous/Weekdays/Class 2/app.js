35fd5709c08c0e10af63eb44518fb18928fc0084
Naima-Ahmad <naim.khan.ann@gmail.com>
2023-12-31 15:19:20 +0500

    first commit

---
+++ Sir ghous/Weekdays/Class 2/app.js
@@ -0,0 +1,213 @@
+// Example 1
+
+// ALERTS
+
+
+
+// alert('Hello World');
+
+
+// Example 2
+
+// alert(234)
+
+
+// Example 3
+
+// alert('234')
+
+
+// Example 4
+
+// alert('Naima')
+
+
+//Example 5
+
+// alert(Naima)
+
+
+// Example 6
+
+//  alert ( 'Naima Ahmad' )
+
+
+// Example 7
+
+// alert('Naima')
+
+
+// Example 8
+
+// window.alert('Hello World !')
+
+
+// Example 9
+
+// var fullName = alert('Naima Ahmad')
+
+// alert(fullName)
+// alert(fullName)
+
+// alert(fullName)
+// alert(fullName)
+
+
+// Example 10
+
+// var fullName = alert('Naima Ahmad')
+
+// alert('fullName')
+
+
+// Example 11
+
+// var fullName = 'Naima Ahmad'
+
+// alert(fullName)
+
+// fullName = 'Laiba Ahmad'
+
+// alert(fullName)
+
+
+// Example 12
+
+// var fullName;
+
+// alert(fullName)
+
+// fullName = 'Laiba Ahmad'
+
+// alert(fullName)
+
+
+// Example 13
+
+// var fullName = 'Naima Ahmad'
+// alert(FULLname)
+
+
+// Example 14
+
+// var fullName = 'Naima Ahmad'
+// alert(FullName)
+
+
+// Example 15
+
+// var fullName = 'Naima Ahmad'
+// alert(fullname)
+
+
+// Example 16
+
+// var num = 10;
+// alert(num);
+
+
+// Example 17
+
+// var num = 10;
+// alert(num + 10);
+
+
+// Example 18
+
+// var num = 10;
+// var inc = num + 10
+// alert(inc);
+
+
+// Example 19
+
+// var num = 20;
+// num = 10;
+// alert(num)
+
+
+// Example 20
+
+// var num = 20;
+// num = num + 10;
+// alert(num)
+
+
+// Example 21
+
+// var num = '20';
+// alert(num + 10)
+
+
+// Example 22
+
+// var num = 20;
+// alert(num + '10')
+
+
+// Example 23
+
+// var num = 20;
+// alert(num + '10' + 5)
+
+
+// Example 24
+
+// var num = 20;
+// alert(num + 10 + '5')
+
+
+// Example 25
+
+// var num = 20;
+
+// alert(num + 10 + 5)
+
+
+// Example 26
+
+// var num = 20;
+// alert(num + 10 + '5' + 10)
+
+
+// Example 27
+
+// var 1st = 'Name'
+// alert(1st)
+
+
+// Example 28
+
+// var full-name = 'Naima Ahmad'
+
+
+// // Example 29
+
+// var full_name = 'Naima Ahmad'
+
+
+// Example 29
+
+// var alert = 'Naima Ahmad'
+
+// alert(alert)
+
+
+// Example 30
+
+// var alert = 'Naima Ahmad'
+
+// console.log(alert)
+
+
+// Example 31
+
+// var var = 'Naima Ahmad'
+
+// alert(alert)
+
+
+// Example 32
+
+var num = 20
+console.log(num + 10)
