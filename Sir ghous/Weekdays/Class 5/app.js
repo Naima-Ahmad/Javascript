3cde5add057e3b3d63e37fb1c1efba47ffc5d6c2
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-01-20 18:44:38 +0500

    Class 5

---
+++ Sir ghous/Weekdays/Class 5/app.js
@@ -0,0 +1,68 @@
+// Example 1
+
+// var student1 = "Naima"
+// var student2 = "Laiba"
+
+// var students = ["Naima","Laiba","Ahmad"]
+
+// console.log(students[2])
+
+
+// Example 2
+
+// var students = ["Naima","Laiba","Ahmad"]
+// students[2] = "Maryam"
+
+// console.log(students)
+
+
+// Example 3
+
+// var students = ["Naima","Laiba","Rabia"]
+// students[3] = "Maryam"
+
+// console.log(students)
+
+
+// Example 4
+
+// var students = ["Naima","Laiba","Rabia"]
+// students[3] = "Maryam"
+// students[4] = "Zunaira"
+// students[5] = "Yashfa"
+
+// console.log(students)
+
+
+// Example 5
+
+// var students = ["Naima","Laiba","Rabia"]
+// students[3] = "Maryam"
+// students[4] = "Zunaira"
+// students[5] = "Yashfa"
+
+// console.log(students.length)
+
+
+// Example 5
+
+// var students = ["Naima","Laiba","Rabia"]
+// students[3] = "Maryam"
+// students[4] = "Zunaira"
+// students[5] = "Yashfa"
+
+// students[students.length - 1] = "Arsheya"
+
+// console.log(students)
+
+
+// Example 5
+
+var students = ["Naima","Laiba","Rabia"]
+students[3] = "Maryam"
+students[4] = "Zunaira"
+students[5] = "Yashfa"
+
+students[students.length - 1] = "Arsheya"
+
+console.log(students)
