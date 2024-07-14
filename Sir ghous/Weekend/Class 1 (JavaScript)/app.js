dd8cf8e221757411b54601f9c3ae2d7ad688ef4b
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-14 21:00:46 +0500

    class 1 practice

--- Sir ghous/Weekend/Class 1 (JavaScript)/app.js
+++ Sir ghous/Weekend/Class 1 (JavaScript)/app.js
@@ -1,50 +1,50 @@
 // Example 1
-// alert("Hello!")
+// alert("Hello!");
 
 // Example 2
-// window.alert("Hello!")
+// window.alert("Hello!");
 
 // Example 3
-// alert ( 'Hello!' )
+// alert ( 'Hello!' );
 
 // Example 4
-// alert(123456)
+// alert(123456);
 
 // Example 5
-// var myName = "Naima Ahmad"
-// alert(myName)
+// var myName = "Naima Ahmad";
+// alert(myName);
 
 // Example 6
 // var myAge;
-// myAge = "12 years old"
-// alert("I am "+myAge)
+// myAge = "12 years old";
+// alert("I am "+myAge);
 
 // Example 7
-// var num1 = 2
-// var num2 = "2"
-// alert(num1+num2)
+// var num1 = 2;
+// var num2 = "2";
+// alert(num1+num2);
 
 // Example 8
 // var myNameIs = 'Naima Ahmad';
-// var iAmInClass = 'in class 6'
-// alert(myNameIs+"\n"+iAmInClass)
+// var iAmInClass = 'in class 6';
+// alert(myNameIs+"\n"+iAmInClass);
 
 // Example 9
 // var 1name = 'Naima';
-// alert(1name)
+// alert(1name);
 
 // Example 10
 // var name1 = "Naima";
 // var name2 = "Ahmad";
-// var fullName = name1+" "+name2
-// alert(fullName)
+// var fullName = name1+" "+name2;
+// alert(fullName);
 
 // Example 11
-// var num1 = 10
-// var num2 = 10
-// alert(num1+num2)
+// var num1 = 10;
+// var num2 = 10;
+// alert(num1+num2);
 
 // Example 12
-var num = 10
-num = num+5
-alert(num)
+var num = 10;
+num = num+5;
+alert(num);
