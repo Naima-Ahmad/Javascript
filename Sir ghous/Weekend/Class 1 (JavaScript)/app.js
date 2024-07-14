de53d42d08335b3a5a1f601920f4e6780f0ae3d8
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-14 20:47:40 +0500

    class 1

---
+++ Sir ghous/Weekend/Class 1 (JavaScript)/app.js
@@ -0,0 +1,16 @@
+// Example 1
+// alert("Hello!")
+
+// Example 2
+// window.alert("Hello!")
+
+// Example 3
+// alert ( 'Hello!' )
+
+// Example 4
+// alert(123456)
+
+// Example 5
+// var myName = "Naima Ahmad"
+// alert(myName)
+
