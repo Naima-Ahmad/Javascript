670d2f92aa72b85778c7813d8d9d315ddefea282
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-15 09:29:32 +0500

    class 2 practice

--- Sir ghous/TechKol/Class 2 ( Javascript )/app.js
+++ Sir ghous/TechKol/Class 2 ( Javascript )/app.js
@@ -13,6 +13,48 @@
 // alert(num1);
 
 // Example 5
-var num2 = 20;
-num2 = num2+5;
-console.log(num2)
+// var num2 = 20;
+// num2 = num2+5;
+// console.log(num2);
+
+// Example 6
+// var num3 = 20;
+// console.log(num3)
+// num3 = 5;
+// console.log(num3)
+// num3 = 64;
+// console.log(num3)
+
+// Example 7
+// var myFavouriteNumberIs = 4;
+// alert(myFavouriteNumberIs);
+// var my_favourite_number_is = 5;
+// alert(my_favourite_number_is);
+
+// Example 8
+// var num4 = 5;
+// alert(num1);
+// var 4num = 4;
+// alert(1num);
+
+// Example 9
+// var var = "Hello";
+// alert(var);
+
+// Example 10
+// alert(2+2);
+
+// Example 11
+// var num5 = "10";
+// var num6 = 20;
+// console.log(num5+num6);
+
+// Example 12
+// var num7 = "10";
+// var num8 = 20;
+// console.log(num7-num8);
+
+// Example 13
+var num9 = "40";
+var num10 = 20;
+console.log(num9/num10);
