0466d83d507221dc0c60a8fb3f2d68767d204111
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-15 09:13:16 +0500

    class 2 practice

---
+++ Sir ghous/TechKol/Class 2 ( Javascript )/app.js
@@ -0,0 +1,18 @@
+// Example 1
+// var mess1 = 'I don't know';
+
+// Example 2
+// var mess2 = "I don't know";
+
+// Example 3
+// var mess3 = "Thank you!"
+// console.log(mess3)
+
+// Example 4
+// var num1 = 20;
+// alert(num1);
+
+// Example 5
+var num2 = 20;
+num2 = num2+5;
+console.log(num2)
