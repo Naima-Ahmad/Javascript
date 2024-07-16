57615c8c775ce56bdff2f2e2281d5878621c31a3
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-16 20:37:21 +0500

    class 2

---
+++ Sir ghous/bath 11/Class 2 ( JavaScript )/app.js
@@ -0,0 +1,14 @@
+// Example 1
+// document.write("Hello I am practicing javascript.")
+
+// Example 2
+// var mess = "Hello World!"
+// document.write("<h1>"+mess+"<h1>")
+
+// Example 3
+// var num1 = 3;
+// num1 = num1++;
+// alert(num1);
+
+// Example 4
+alert(10+10);
