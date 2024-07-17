97980d653b72b7a993352931b2f51a6468c19420
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-17 19:59:48 +0500

    class 2

--- Sir ghous/Weekend/Class 2 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 2 ( JavaScript )/app.js
@@ -1,4 +1,24 @@
 // Example 1
-var num = 5;
-num = num + 1;
-console.log(num)
+// var num = 5;
+// num = num + 1;
+// console.log(num)
+
+// Example 2
+// var num1 = 2;
+// var num2 = "2";
+// console.log(num1+num2);
+
+// Example 3
+// var num1 = "2";
+// var num2 = 2;
+// console.log(num1+num2);
+
+// Example 4
+// var num1 = "2";
+// var num2 = 2;
+// console.log(num1*num2);
+
+// Example 5
+var mess = "Hello";
+var 1 = 2;
+console.log(num1*num2);
