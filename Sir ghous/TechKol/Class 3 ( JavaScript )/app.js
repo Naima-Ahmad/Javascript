b84263a00e3a3a3f46ed48c4784c43c3bfda8af7
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-18 19:12:08 +0500

    class 2

--- Sir ghous/TechKol/Class 3 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 3 ( JavaScript )/app.js
@@ -1,3 +1,24 @@
 // Example 1
-var num = 12;
-console.log(num++);
+// var num = 12;
+// console.log(num++);
+// var num1 = 12;
+// console.log(++num1);
+
+// Example 2
+// var num = 2;
+// var total = num++ + --num + ++num - num--;
+// console.log(total);
+
+// Example 3
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log(result)
+
+// Example 4
+// var num = 1 + 3 * 4;
+// console.log(num);
+
+// Example 5
+var num = (((2 * 4) + 8 - 4) + 10);
+console.log(num);   
