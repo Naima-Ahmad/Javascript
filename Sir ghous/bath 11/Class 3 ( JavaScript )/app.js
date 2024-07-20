9ef7763af0ec7d1da91617656b079090264c9803
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-20 19:26:30 +0500

    class 3

---
+++ Sir ghous/bath 11/Class 3 ( JavaScript )/app.js
@@ -0,0 +1,20 @@
+// Example 1
+// var firstName = "Naima";
+// var lastName = "Ahmad";
+// console.log(firstName + " " + lastName);
+
+// Example 2
+// var num = "2";
+// var num2 = "2";
+// console.log(num+num2);
+// var num = 2;
+// var num2 = "2";
+// console.log(num+num2);
+// var num = "2";
+// var num2 = 2;
+// console.log(num+num2);
+
+// Example 3
+// var num = "2";
+// var num2 = 2;
+// console.log(num+num2+2);
