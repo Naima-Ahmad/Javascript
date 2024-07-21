96f2733872ba174eec20d08ca922cc06f5a59545
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-21 08:22:58 +0500

    class 3

--- Sir ghous/Weekend/Class 3 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 3 ( JavaScript )/app.js
@@ -4,18 +4,53 @@
 // console.log(firstNum + secondNum);
 
 // Example 2
-var num1 = +prompt("First Number:");
-var num2 = +prompt("Second Number:");
-var opr = prompt("Operator:");
-if(opr == "+"){
-    document.write("The sum of "+num1+" + "+num2+" = ",num1+num2);
+// var num1 = +prompt("First Number:");
+// var num2 = +prompt("Second Number:");
+// var opr = prompt("Operator:");
+// if(opr == "+"){
+//     document.write("The sum of "+num1+" + "+num2+" = ",num1+num2);
+// }
+// else if (opr == "-"){
+//     document.write("The sub of "+num1+" - "+num2+" = ",num1-num2);
+// }
+// else if (opr == "*"){
+//     document.write("The sub of "+num1+" * "+num2+" = ",num1*num2);
+// }
+// else if (opr == "/"){
+//     document.write("The sub of "+num1+" / "+num2+" = "+num1/num2);
+// }
+
+// Example 3
+var color = prompt("Enter traffic light signal colour:");
+if(color == "red"){
+    Swal.fire({
+        imageUrl: "./images/file (1).png",
+        title: "Red light on",
+        text: "Wait a Minute",
+    });
 }
-else if (opr == "-"){
-    document.write("The sub of "+num1+" - "+num2+" = ",num1-num2);
+
+else if (color == "yellow"){
+    Swal.fire({
+        imageUrl: "./images/file (2).png",
+        title: "Yellow light on",
+        text: "Now Ready",
+    });
 }
-else if (opr == "*"){
-    document.write("The sub of "+num1+" * "+num2+" = ",num1*num2);
+
+else if (color == "green"){
+    Swal.fire({
+        imageUrl: "./images/file (3).png",
+        title: "Green light on",
+        text: "Now Go",
+    });
 }
-else if (opr == "/"){
-    document.write("The sub of "+num1+" / "+num2+" = "+num1/num2);
+
+else{
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>'
+      });
 }
