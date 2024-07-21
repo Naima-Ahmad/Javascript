bf704ccff48bc25af041d27910d7cc2703d5da14
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-21 17:29:43 +0500

    class 4

--- Sir ghous/TechKol/Class 4 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 4 ( JavaScript )/app.js
@@ -1,3 +1,7 @@
 // Example 1
-var Name = prompt("Enter your Name: ","Naima Ahmad");
-console.log(Name);
+// var Name = prompt("Enter your Name: ","Naima Ahmad");
+// console.log(Name);
+
+// Example 2
+var phone = +prompt("Enter your Phone Number:","+923");
+console.log(phone);
