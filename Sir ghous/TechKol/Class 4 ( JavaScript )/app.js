fcd1cebc7656bee697a8c91e6fdb23ae47bb6aa9
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-21 17:34:29 +0500

    class 4

--- Sir ghous/TechKol/Class 4 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 4 ( JavaScript )/app.js
@@ -7,10 +7,15 @@
 // console.log(phone);
 
 // Example 3
-var marks = 50;
-if(marks >= 50){
-    console.log("Pass")
-}
-else{
-    console.log("Fail")
-}
+// var marks = +prompt("Enter your Marks:");
+// if(marks >= 50){
+//     console.log("Pass")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// Example 4
+var firstNum = +prompt("Enter a first number:");
+var secondNum = +prompt("Enter a second number:");
+console.log(firstNum+secondNum);
