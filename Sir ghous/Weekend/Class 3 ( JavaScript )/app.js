51162e9405163be12a1897a33d91e2e17685eb4d
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-21 07:57:36 +0500

    class 3

--- Sir ghous/Weekend/Class 3 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 3 ( JavaScript )/app.js
@@ -1,4 +1,21 @@
 // Example 1
-var firstNum = +prompt("Enter first Number:");
-var secondNum = +prompt("Enter second Number:");
-console.log(firstNum + secondNum);
+// var firstNum = +prompt("Enter first Number:");
+// var secondNum = +prompt("Enter second Number:");
+// console.log(firstNum + secondNum);
+
+// Example 2
+var num1 = +prompt("First Number:");
+var num2 = +prompt("Second Number:");
+var opr = prompt("Operator:");
+if(opr == "+"){
+    document.write("The sum of "+num1+" + "+num2+" = ",num1+num2);
+}
+else if (opr == "-"){
+    document.write("The sub of "+num1+" - "+num2+" = ",num1-num2);
+}
+else if (opr == "*"){
+    document.write("The sub of "+num1+" * "+num2+" = ",num1*num2);
+}
+else if (opr == "/"){
+    document.write("The sub of "+num1+" / "+num2+" = "+num1/num2);
+}
