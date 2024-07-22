bf5ba1f4faf58f2ec716da8dfd5df470fdc25c2a
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-22 20:47:59 +0500

    class 4

--- Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
@@ -15,9 +15,24 @@ var arr = ["Rawalpindi", "Islamabad", "Karachi"];
 // console.log(arr[3]);
 
 // Example 3
-var arr = ["Rawalpindi", "Islamabad", "Karachi"];
-console.log(arr[0]);
-console.log(arr[3]);
-arr.push("Lahore")
-console.log(arr[3]);
-arr.push("Lahore", "Multan", "Peshawar");
+// var arr = ["Rawalpindi", "Islamabad", "Karachi"];
+// console.log(arr[0]);
+// console.log(arr[3]);
+// arr.push("Lahore")
+// console.log(arr[3]);
+// arr.push("Lahore", "Multan", "Peshawar");
+
+// Example 4
+// var city = [];
+// var usercity = prompt("Enter your city Name: ")
+// city.push(usercity);
+// console.log(city);
+
+// Example 5
+var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+console.log(arr[0,3])
+arr.splice(2,2);
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
