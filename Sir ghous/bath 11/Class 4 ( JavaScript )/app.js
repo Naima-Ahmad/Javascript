35712f2f8fbcaf925478f2ac9dec662a250e34a8
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-22 20:01:42 +0500

    class 4

---
+++ Sir ghous/bath 11/Class 4 ( JavaScript )/app.js
@@ -0,0 +1,35 @@
+// Example 1
+// alert("Naima");
+// alert(2+2);
+
+// Example 2
+// console.log(2+2);
+
+// Example 3
+// var fullName = "Naima";
+// var fullName = "Naima Ahmad";
+// console.log(fullName)
+
+// Example 4
+// var firstName = "Naima";
+// var lastName = "Ahmad";
+// console.log(firstName+" "+lastName);
+
+// Example 5
+// var num1 = 20;
+// var num2 = 30;
+// var restult = num1+num2;
+// console.log(restult)
+
+// Example 6
+var num1 = 20;
+var num2 = 30;
+var plus = num1+num2;
+var minus = num1-num2;
+var multiply = num1*num2;
+var divide = num1/num2;
+
+console.log(num1+" + "+num2+" = ",plus);
+console.log(num1+" - "+num2+" = ",minus);
+console.log(num1+" * "+num2+" = ",multiply);
+console.log(num1+" / "+num2+" = ",divide);
