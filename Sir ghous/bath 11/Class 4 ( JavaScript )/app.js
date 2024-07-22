23aff3525c2923f682488dfed41a9d84805d31e0
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-22 20:12:09 +0500

    class 4

--- Sir ghous/bath 11/Class 4 ( JavaScript )/app.js
+++ Sir ghous/bath 11/Class 4 ( JavaScript )/app.js
@@ -22,14 +22,34 @@
 // console.log(restult)
 
 // Example 6
-var num1 = 20;
-var num2 = 30;
-var plus = num1+num2;
-var minus = num1-num2;
-var multiply = num1*num2;
-var divide = num1/num2;
+// var num1 = 20;
+// var num2 = 30;
+// var plus = num1+num2;
+// var minus = num1-num2;
+// var multiply = num1*num2;
+// var divide = num1/num2;
+// console.log(num1+" + "+num2+" = ",plus);
+// console.log(num1+" - "+num2+" = ",minus);
+// console.log(num1+" * "+num2+" = ",multiply);
+// console.log(num1+" / "+num2+" = ",divide);
 
-console.log(num1+" + "+num2+" = ",plus);
-console.log(num1+" - "+num2+" = ",minus);
-console.log(num1+" * "+num2+" = ",multiply);
-console.log(num1+" / "+num2+" = ",divide);
+// Example 7
+// var a = 5;
+// var b = a++; // post increment
+// console.log("a = ",a);
+// console.log("b = ",b);
+// console.log("");
+// var a = 5;
+// var b = ++a; // pre increment
+// console.log("a = ",a);
+// console.log("b = ",b);
+
+// Example 8
+var num1 = 10;
+var num2 = 5;
+if(num1 > num2){
+    console.log(num1+" is greater than "+num2)
+}
+else{
+    console.log(num1+" is less than "+num2)
+}
