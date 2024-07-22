3eba35afb222beb1848e41fc74ab4e545cc1c703
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-07-22 20:41:51 +0500

    class 4

--- Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
@@ -1,6 +1,23 @@
 // Example 1
 var arr = ["Rawalpindi", "Islamabad", "Karachi"];
+// console.log(arr[0]);
+// console.log(arr[3]);
+// arr[3] = "Lahore";
+// console.log(arr[3]);
+
+// Example 2
+// var arr = ["Rawalpindi", "Islamabad", "Karachi"];
+// console.log(arr[0]);
+// arr[3] = "Lahore";
+// console.log(arr[3]);
+// arr.pop()
+// arr.pop()
+// console.log(arr[3]);
+
+// Example 3
+var arr = ["Rawalpindi", "Islamabad", "Karachi"];
 console.log(arr[0]);
 console.log(arr[3]);
-arr[3] = "Lahore";
+arr.push("Lahore")
 console.log(arr[3]);
+arr.push("Lahore", "Multan", "Peshawar");
