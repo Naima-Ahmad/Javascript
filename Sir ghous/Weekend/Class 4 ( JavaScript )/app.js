bd2f11e7fcbee731f4c0d77c6df0f782670021f7
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-01 20:23:47 +0500

    class 4

--- Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
+++ Sir ghous/Weekend/Class 4 ( JavaScript )/app.js
@@ -29,10 +29,42 @@ var arr = ["Rawalpindi", "Islamabad", "Karachi"];
 // console.log(city);
 
 // Example 5
+// var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+// console.log(arr[0,3])
+// arr.splice(2,2);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+
+// Example 6
+// var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+// arr.splice(1,0, "Quetta")
+// console.log(arr)
+// arr.splice(2,1)
+// console.log(arr)
+
+// Example 7
+// var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+// var copyArr = arr.slice(1,3);
+// console.log(copyArr)
+
+// Example 8
+// var fullName = "Naima Ahmad";
+// var lastName = fullName.slice(6,11)
+// console.log(lastName)
+
+// Example 9
+// var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+// var copyArr = arr.slice(-1);
+// console.log(copyArr)
+
+// Example 10
+// var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
+// var returnValue = arr.pop();
+// console.log(returnValue)
+
+// Example 11
 var arr = ["Rawalpindi", "Islamabad", "Karachi", "Peshawar"];
-console.log(arr[0,3])
-arr.splice(2,2);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
+var returnValue = arr.pop();
+console.log(returnValue)
