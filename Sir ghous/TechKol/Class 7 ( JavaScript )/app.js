1cb11c0791f36844dc4c00c02497ae28565ababc
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-14 10:49:56 +0500

    class 7

---
+++ Sir ghous/TechKol/Class 7 ( JavaScript )/app.js
@@ -0,0 +1,13 @@
+// Example 1
+// var names = ["ghous"];
+// names[names.length] = "ahmed";
+// console.log(names)
+// names[names.length] = "khan";
+// console.log(names)
+
+// Example 2
+var names = ["ghous"];
+names[names.length] = "ahmed";
+names[names.length] = "khan";
+names.pop()
+console.log(names)
