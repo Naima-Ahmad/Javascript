fde37170c9fb58986a4f23123e2d8dd1e51211fc
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-14 10:59:22 +0500

    class 7

--- Sir ghous/TechKol/Class 7 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 7 ( JavaScript )/app.js
@@ -6,8 +6,34 @@
 // console.log(names)
 
 // Example 2
-var names = ["ghous"];
-names[names.length] = "ahmed";
-names[names.length] = "khan";
-names.pop()
-console.log(names)
+// var names = ["ghous"];
+// names[names.length] = "ahmed";
+// names[names.length] = "khan";
+// names.pop()
+// console.log(names)
+ 
+// Example 3
+// var names = ["ghous"];
+// names[names.length] = "ahmed";
+// names[names.length] = "khan";
+// names.pop()
+// console.log(names)
+// names.push("Saad")
+// names.push("Hussain")
+// console.log(names)
+// names.push("Hassan", "Zubair", "Tahir")
+// console.log(names)
+
+// Example 4
+// var arr = ['ghous', 'ahmed', 'tahir', 'adil']
+// console.log(arr)
+// arr.shift()
+// arr.shift()
+// console.log(arr)
+
+// Example 5
+var arr = ['ghous', 'ahmed', 'tahir', 'adil']
+console.log(arr)
+arr.unshift('amir')
+arr.unshift('hussain', 'hassan', 'saim')
+console.log(arr)
