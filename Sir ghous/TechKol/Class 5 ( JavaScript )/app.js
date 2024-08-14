594eca03121c7de6bb44232ed528c29f576c3a8b
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-14 08:13:29 +0500

    class

---
+++ Sir ghous/TechKol/Class 5 ( JavaScript )/app.js
@@ -0,0 +1,20 @@
+// Example 1
+// var lang = prompt("Enter learned programming language:");
+// if(lang === "HTML"){
+//     alert("You are hired! 📢🎉🥳");
+// }
+// else{
+//     alert("Not hired! 😐😐😐")
+// }
+
+// Example 2
+var food = prompt("Write the name of the food you are bringing from the market:")
+if(food == "vegetables"){
+    alert("Yes 👍")
+}
+else if (food == "chicken"){
+    alert("Yummy 😋")
+}
+else{
+    alert("No 👎")
+}
---
+++ Sir ghous/TechKol/Class 6 ( JavaScript )/app.js
@@ -0,0 +1,6 @@
+// Example 1
+var student1 = "ghous";
+var student2 = "ahmed";
+var students = ["ghous", "ahmed", "ali", "raza", "khan"];
+console.log(students);
+alert(students)
