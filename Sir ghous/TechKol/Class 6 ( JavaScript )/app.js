034ca6a7a49d091556feb4daf4b41a9629bae43e
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-14 08:20:47 +0500

    class 6

--- Sir ghous/TechKol/Class 6 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 6 ( JavaScript )/app.js
@@ -1,6 +1,21 @@
 // Example 1
+// var student1 = "ghous";
+// var student2 = "ahmed";
+// var students = ["ghous", "ahmed", "ali", "raza", "khan"];
+// console.log(students);
+// alert(students)
+
+// Example 2
+// var student1 = "ghous";
+// var student2 = "ahmed";
+// var students = ["ghous", "ahmed", "ali", "raza", "khan"];
+// console.log(students[2]);
+// console.log(students[3]);
+// console.log(students[4]);
+
+// Example 3
 var student1 = "ghous";
 var student2 = "ahmed";
 var students = ["ghous", "ahmed", "ali", "raza", "khan"];
-console.log(students);
-alert(students)
+console.log(students[0] + " " + students[1]);
+console.log(students[0] , " " , students[1]);
