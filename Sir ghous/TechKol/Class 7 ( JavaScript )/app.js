7b0336fa597de072cb8c79323d056f5886279c70
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-16 17:51:06 +0500

    class 7

--- Sir ghous/TechKol/Class 7 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 7 ( JavaScript )/app.js
@@ -32,8 +32,26 @@
 // console.log(arr)
 
 // Example 5
-var arr = ['ghous', 'ahmed', 'tahir', 'adil']
-console.log(arr)
-arr.unshift('amir')
-arr.unshift('hussain', 'hassan', 'saim')
-console.log(arr)
+// var arr = ['ghous', 'ahmed', 'tahir', 'adil']
+// console.log(arr)
+// arr.unshift('amir')
+// arr.unshift('hussain', 'hassan', 'saim')
+// console.log(arr)
+
+// Example 6
+// var fruits = ['apple', 'mango', 'banana', 'orange'];
+// fruits.splice(2,1)
+// console.log(fruits)
+// fruits.splice(2,2)
+// console.log(fruits)
+
+// Example 7
+// var fruits = ['apple', 'mango', 'banana', 'orange'];
+// fruits.splice(2, 0, "grapes")
+// console.log(fruits)
+// fruits.splice(1, 0, "kiwi", "watermelon", 'dates')
+// console.log(fruits)
+
+// Example 8
+var num = 100_00_00_00_00;
+console.log(num)
