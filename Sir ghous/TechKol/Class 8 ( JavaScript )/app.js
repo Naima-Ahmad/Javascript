2a4e4fcdd7501caaadb71984fa6e2e10446792f2
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-17 05:56:27 +0500

    Class 7

---
+++ Sir ghous/TechKol/Class 8 ( JavaScript )/app.js
@@ -0,0 +1,22 @@
+// Example 1
+// var arr = [2, 6, 4, 1, 3, 5];
+// console.log(arr.sort())
+
+// Example 2
+// var arr = ['Hammad', 'Jawad', 'Hashir', 'Haris', 'Zohaib'];
+// console.log(arr.join())
+// console.log(arr.join('&'))
+// console.log(arr.join(' '))
+
+// Example 3
+// var str = "Hammad&Jawad&Hashir&Haris&Zohaib";
+// console.log(str.split('&'))
+// var str = "Hammad Jawad Hashir Haris Zohaib";
+// console.log(str.split(' '))
+// var str = "Hammad Jawad-Hashir=Haris Zohaib";
+// console.log(str.split(' '));
+
+// Example 4
+var arr = ['Hammad', 'Jawad', 'Hashir', 'Haris', 'Zohaib'];
+var met = arr.reverse();
+console.log(met);
