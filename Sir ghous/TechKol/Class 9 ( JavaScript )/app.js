9ea738536907d482c1ee978f908aa4f0071338b2
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-21 20:29:04 +0500

    class 9

---
+++ Sir ghous/TechKol/Class 9 ( JavaScript )/app.js
@@ -0,0 +1,12 @@
+// Example 1
+// var str = prompt("Enter a Word: ");
+// console.log(str.split("").reverse().join(""));
+
+// Example 2
+for(var i = 1; i < 5; i++){
+    for(var j = 1; j < 5; j++){
+        console.log(i,j)
+        break
+    }
+    break
+}
