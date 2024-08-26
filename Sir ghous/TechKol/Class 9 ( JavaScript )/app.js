3d47e02306e66e4778c692d72297f8c94ebaadba
Naima-Ahmad <naim.khan.ann@gmail.com>
2024-08-26 18:35:17 +0500

    class 9

--- Sir ghous/TechKol/Class 9 ( JavaScript )/app.js
+++ Sir ghous/TechKol/Class 9 ( JavaScript )/app.js
@@ -3,10 +3,28 @@
 // console.log(str.split("").reverse().join(""));
 
 // Example 2
-for(var i = 1; i < 5; i++){
-    for(var j = 1; j < 5; j++){
-        console.log(i,j)
-        break
-    }
-    break
+// for(var i = 1; i < 5; i++){
+//     for(var j = 1; j < 5; j++){
+//         console.log(i,j)
+//         break
+//     }
+//     break
+// }
+
+// Example 3
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// for(var i = 0; i < firstNames.length; i++){
+//     for(var j = 0; j < lastNames.length; j++){
+//     console.log(firstNames[i] +" "+ lastNames[j]);
+//     }
+// }
+
+// Example 4
+var arr = [["ahmed","ali","raza"]]
+for(var i = 0; i < arr.length; i++){
+    console.log(arr[i])
+    // for(var j = 0; j < arr[i].length; j++){
+    //     console.log(arr[i][j])
+    // }
 }
